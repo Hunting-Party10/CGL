e2eeeb192a117c466bba0274ba0e005c5de0ce38
Atharva Joshi <athu200299@gmail.com>
2019-03-23 15:05:58 +0530

    Assignment 4

--- Assignment4/Assignment4.c
+++ Assignment4/Assignment4.c
@@ -2,8 +2,9 @@
 #include <GL/glut.h>
 #include <stdio.h>
 #include <stdlib.h>
-GLint xstart=50,ystart=50;
-GLint xend=450,yend=450;
+#define shift 100
+GLint xstart=25,ystart=25;
+GLint xend=225,yend=225;
 
 typedef struct pixel
 {
@@ -27,11 +28,13 @@ void boundry_fill(int x ,int y, pixel boundrycolor, pixel fillcolor)
 		glEnd();
 		glFlush();
 		boundry_fill(x+1,y, boundrycolor, fillcolor);
-		boundry_fill(x-2,y, boundrycolor, fillcolor);
-	  boundry_fill(x,y+2, boundrycolor, fillcolor);
-	  boundry_fill(x,y-2, boundrycolor, fillcolor);
+		//boundry_fill(x-2,y, boundrycolor, fillcolor);
+	  boundry_fill(x,y+1, boundrycolor, fillcolor);
+	  //boundry_fill(x,y-2, boundrycolor, fillcolor);
 	}
 }
+
+
 
 void BresenhamLine(int x1,int y1,int x2,int y2){
 	int dy=abs(y2-y1),dx=abs(x2-x1);
@@ -76,10 +79,8 @@ void BresenhamLine(int x1,int y1,int x2,int y2){
 	glFlush();
 
 }
-
-void render(){
-
-
+void drawbase()
+{
 	pixel oldcolor;
 	oldcolor.red =1.0;
 	oldcolor.green = 0.0;
@@ -91,33 +92,56 @@ void render(){
 	newcolor.blue = 0.0;
 
 	glColor3f(oldcolor.red,oldcolor.green,oldcolor.blue);	//Color to use for for drawing objects - (R,G,B) values
-	BresenhamLine(50,50,450,50);
-	BresenhamLine(50,150,450,150);
-	BresenhamLine(50,250,450,250);
-	BresenhamLine(50,350,450,350);
-	BresenhamLine(50,450,450,450);
-	BresenhamLine(50,50,50,450);
-	BresenhamLine(150,50,150,450);
-	BresenhamLine(250,50,250,450);
-	BresenhamLine(350,50,350,450);
-	BresenhamLine(450,50,450,450);
+	BresenhamLine(xstart + shift,ystart + shift,xend + shift,ystart + shift);
+	BresenhamLine(xstart + shift,75 + shift,xend + shift,75 + shift);
+	BresenhamLine(xstart + shift,125 + shift,xend + shift,125 + shift);
+	BresenhamLine(xstart + shift,175 + shift,xend + shift,175 + shift);
+	BresenhamLine(xstart + shift,yend + shift,xend + shift,yend + shift);
+	BresenhamLine(xstart + shift,ystart + shift,xstart + shift,yend + shift);
+	BresenhamLine(75 + shift,ystart + shift,75 + shift,yend + shift);
+	BresenhamLine(125 + shift,ystart + shift,125 + shift,yend + shift);
+	BresenhamLine(175 + shift,ystart + shift,175 + shift,yend + shift);
+	BresenhamLine(xend + shift,ystart + shift,xend + shift,yend + shift);
+	boundry_fill(xstart + 1 + shift,ystart + 1 + shift,oldcolor,newcolor);
+	boundry_fill(126 + shift,ystart +1 + shift,oldcolor,newcolor);
+	boundry_fill(76 + shift,76 + shift,oldcolor,newcolor);
+	boundry_fill(176 + shift,76 + shift,oldcolor,newcolor);
+	boundry_fill(xstart + 1 + shift,126 + shift,oldcolor,newcolor);
+	boundry_fill(126 + shift,126 + shift,oldcolor,newcolor);
+	boundry_fill(76 + shift,176 + shift,oldcolor,newcolor);
+	boundry_fill(176 + shift,176 + shift,oldcolor,newcolor);
+}
+void render(){
+	drawbase();
+}
 
-
-	boundry_fill(51,51,oldcolor,newcolor);
+void rotate()
+{
 
 }
 
+void mousePoint(GLint button,GLint action,GLint xMouse,GLint yMouse){
+	if(button == GLUT_LEFT_BUTTON || (button == GLUT_LEFT_BUTTON) && action == GLUT_DOWN)
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+	}
+	if(button == GLUT_LEFT_BUTTON && action == GLUT_UP)
+		drawbase();
+	if(button == GLUT_RIGHT_BUTTON && action == GLUT_UP)
+		rotate();
+}
 
 int main(int argc, char **argv)
 {
 	glutInit(&argc,argv); // Initialise GLUT library
-  	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);   	// Set the initial display mode
-  	glutInitWindowSize(500,500);
-  	glutInitWindowPosition(250,250); // Set the initial window position
-  	glutCreateWindow("Assignment 4"); // Create window with title Assignment4
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);   	// Set the initial display mode
+  glutInitWindowSize(500,500);
+  glutInitWindowPosition(250,250); // Set the initial window position
+  glutCreateWindow("Assignment 4"); // Create window with title Assignment4
 	glClearColor(1.0,1.0,1.0,0.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 	gluOrtho2D(0,500,0,500);
 	glutDisplayFunc(render);
+	glutMouseFunc(mousePoint);
 	glutMainLoop();
 }
