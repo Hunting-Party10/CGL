2cbe96e2a75555e2f42a02eafa13bab181d250e7
Atharva Joshi <athu200299@gmail.com>
2019-03-29 10:59:12 +0530

    Update

--- Assignment4/Assignment4.c
+++ Assignment4/Assignment4.c
@@ -2,10 +2,13 @@
 #include <GL/glut.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #define shift 100
 GLint xstart=125,ystart=125;
 GLint xend,yend;
 GLint size;
+double point[20][2];
+double transform[2][2];
 
 typedef struct pixel
 {
@@ -18,7 +21,6 @@ void boundry_fill(int x ,int y, pixel boundrycolor, pixel fillcolor)
 {
 	pixel c;
 	glReadPixels(x,y,1,1,GL_RGB,GL_FLOAT, &c);
-	//printf("%f - %f - %f\n",c.red,c.blue,c.green );
 	if((c.red != boundrycolor.red || c.green != boundrycolor.green || c.blue !=
                boundrycolor.blue) && (c.red != fillcolor.red || c.green != fillcolor.green || c.blue !=
 						                fillcolor.blue))
@@ -29,9 +31,8 @@ void boundry_fill(int x ,int y, pixel boundrycolor, pixel fillcolor)
 		glEnd();
 		glFlush();
 		boundry_fill(x+1,y, boundrycolor, fillcolor);
-		//boundry_fill(x-2,y, boundrycolor, fillcolor);
-	  boundry_fill(x,y+1, boundrycolor, fillcolor);
-	  //boundry_fill(x,y-2, boundrycolor, fillcolor);
+		boundry_fill(x,y+1, boundrycolor, fillcolor);
+
 	}
 }
 void BresenhamLine(int x1,int y1,int x2,int y2){
@@ -79,7 +80,8 @@ void BresenhamLine(int x1,int y1,int x2,int y2){
 }
 void drawbase()
 {
-
+	
+	glClear(GL_COLOR_BUFFER_BIT);
 	pixel oldcolor;
 	oldcolor.red =1.0;
 	oldcolor.green = 0.0;
@@ -94,11 +96,25 @@ void drawbase()
 	int offset = 0;
 	xend =xstart + size;
 	yend = ystart + size;
+	int k = 0;
 	glColor3f(oldcolor.red,oldcolor.green,oldcolor.blue);
 	for(i = 0;i<5;i++)
 	{
 		BresenhamLine(xstart + offset,ystart,xstart + offset,yend);
+		point[k][0] = xstart + offset - xstart;
+		point[k][1] =ystart - ystart;
+		k++;
+		point[k][0] = xstart + offset - xstart;
+		point[k][1] = yend - ystart;
+		k++;
 		BresenhamLine(xstart,ystart + offset,xend,ystart + offset);
+		point[k][0] = xstart - xstart;
+		point[k][1] = ystart + offset - ystart;
+		k++;
+		point[k][0] = xend - xstart;
+		point[k][1] = ystart + offset - ystart;
+		k++;
+		
 		offset += block;
 	}
 	offset =block;
@@ -109,73 +125,65 @@ void drawbase()
 	boundry_fill(xstart + offset + 1,y + 1,oldcolor,newcolor);
 	boundry_fill(xstart + offset + offset + offset + 1,y + 1,oldcolor,newcolor);
 	y+=block;
-//	boundry_fill(xstart+1,y+1,oldcolor,newcolor);
-//	boundry_fill(xstart + 2*offset+1,y+1,oldcolor,newcolor);
+	boundry_fill(xstart+1,y+1,oldcolor,newcolor);
+	boundry_fill(xstart + 2*offset+1,y+1,oldcolor,newcolor);
 	y+=block;
-//	boundry_fill(xstart + offset+1,y+1,oldcolor,newcolor);
-//	boundry_fill(xstart + offset + offset + offset + 1,y + 1,oldcolor,newcolor);
-/*
-	for(i = 1; i<=4;i++)
-	{
-		if(i%2 != 0)
-		{
-			boundry_fill(xstart+1,y+1,oldcolor,newcolor);
-			boundry_fill(xstart + 2*offset+1,y+1,oldcolor,newcolor);
-		}
-		else
-		{
-			boundry_fill(xstart + offset+1,y+1,oldcolor,newcolor);
-			boundry_fill(xstart + 3*offset+1,y+1,oldcolor,newcolor);
-		}
-		y+=block;
-	}
+	boundry_fill(xstart + offset+1,y+1,oldcolor,newcolor);
+	boundry_fill(xstart + offset + offset + offset + 1,y + 1,oldcolor,newcolor);
 
-		//Color to use for for drawing objects - (R,G,B) values
-	boundry_fill(xstart + 1 + shift,ystart + 1 + shift,oldcolor,newcolor);
-	boundry_fill(126 + shift,ystart +1 + shift,oldcolor,newcolor);
-	boundry_fill(76 + shift,76 + shift,oldcolor,newcolor);
-	boundry_fill(176 + shift,76 + shift,oldcolor,newcolor);
-	boundry_fill(xstart + 1 + shift,126 + shift,oldcolor,newcolor);
-	boundry_fill(126 + shift,126 + shift,oldcolor,newcolor);
-	boundry_fill(76 + shift,176 + shift,oldcolor,newcolor);
-	boundry_fill(176 + shift,176 + shift,oldcolor,newcolor);
-	*/
 }
 void render(){
 	printf("Enter Chess Board Size :");
 	scanf("%d",&size);
-	drawbase();
 }
 
 void rotate()
 {
 
+	pixel oldcolor;
+	oldcolor.red =1.0;
+	oldcolor.green = 0.0;
+	oldcolor.blue = 0.0;
+
+	pixel newcolor;
+	newcolor.red =0.0;
+	newcolor.green = 0.0;
+	newcolor.blue = 0.0;
+	glClear(GL_COLOR_BUFFER_BIT);
+	printf("%lf-%lf\n",point[0][0],point[0][1]);
+	printf("%lf-%lf\n",point[1][0],point[1][1]);
+	transform[0][0] = transform[0][1] = transform[1][0] = transform [1][1] = 1/sqrt(2);
+	transform[1][0] *= -1;
+	int i;
+	glColor3f(oldcolor.red,oldcolor.green,oldcolor.blue);
+	for(i = 0; i<20;i++)
+	{
+		point[i][0] = (point[i][0] * transform[0][0] + point[i][1] * transform[1][0]);
+		point[i][1] = (point[i][0] * transform[0][1] + point[i][1] * transform[1][1]); 
+	}
+	printf("%lf-%lf\n",point[0][0],point[0][1]);
+	printf("%lf-%lf\n",point[1][0],point[1][1]);
+	for(i = 0;i<1;i+=2)
+	{
+		BresenhamLine(point[i][0] + xstart,point[i][1] +ystart,point[i+1][0]+ xstart,point[i+1][1]+ ystart);
+	}
+	
 }
 
 void mousePoint(GLint button,GLint action,GLint xMouse,GLint yMouse){
-/*	if(button == GLUT_LEFT_BUTTON || (button == GLUT_LEFT_BUTTON) && action == GLUT_DOWN)
-	{
-		glClear(GL_COLOR_BUFFER_BIT);
-	}
-
 	if(button == GLUT_RIGHT_BUTTON && action == GLUT_UP)
 		rotate();
-*/
-	if(button == GLUT_LEFT_BUTTON && action == GLUT_DOWN)
-	{
+	if(button == GLUT_LEFT_BUTTON && action == GLUT_UP)
 		drawbase();
-		printf("Left Mouse Clicked");
-	}
-
 }
 
 int main(int argc, char **argv)
 {
-	glutInit(&argc,argv); // Initialise GLUT library
+	glutInit(&argc,argv);// Initialise GLUT library
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);   	// Set the initial display mode
-  glutInitWindowSize(500,500);
-  glutInitWindowPosition(250,250); // Set the initial window position
-  glutCreateWindow("Assignment 4"); // Create window with title Assignment4
+	glutInitWindowSize(500,500);
+  	glutInitWindowPosition(250,250); // Set the initial window position
+  	glutCreateWindow("Assignment 4"); // Create window with title Assignment4
 	glClearColor(1.0f,1.0f,1.0f,0.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 	gluOrtho2D(0,500,0,500);
